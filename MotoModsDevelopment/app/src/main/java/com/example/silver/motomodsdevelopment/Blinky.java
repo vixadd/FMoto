40c889032bc932f95a2aae026a4cebf8d2c31263
vixadd <dkroell@acm.org>
2017-11-22 01:52:11 -0500

    Add development package to use as a sample
    Development package sample addition.

---
+++ MotoModsDevelopment/app/src/main/java/com/example/silver/motomodsdevelopment/Blinky.java
@@ -0,0 +1,13 @@
+package com.example.silver.motomodsdevelopment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Blinky extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blinky);
+    }
+}
