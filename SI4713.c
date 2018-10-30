aad2654ee3f676c0c6ecaf49131e45740aa6c0f4
kroelld <kroelld.96@gmail.com>
2018-10-30 18:17:17 -0400

    Create SI4713.c

---
+++ SI4713.c
@@ -0,0 +1,9 @@
+//////////////////////////////////////////////
+//     Driver for the SI4713 device over    //
+//        I2C and I2S connections.          //
+//                                          //
+//            Author: David Kroell          //
+//           Version: 0.0.1                 //
+//////////////////////////////////////////////
+
+#include <iostream>
